6f11b27c8089a11cb918f41bbd17ae1486212434
Luis Yallico <yallico92@gmail.com>
2025-06-27 11:28:28 +0100

    OTA config skip name check

--- src/components/https/https.c
+++ src/components/https/https.c
@@ -80,56 +80,6 @@ esp_err_t https_get(const char *url, http_response_t *response, const uint8_t *c
     return err;
 }
 
-// esp_err_t https_put(const char *url, const char *data, size_t data_len) {
-//     esp_http_client_config_t config = {
-//         .url = url,
-//         .cert_pem = (char *)server_cert_pem_start,
-//         .timeout_ms = 9000,
-//         .event_handler = http_handler_metadata,
-//     };
-
-//     //ESP_LOGI(TAG, "Using cert_pem at address: %p", server_cert_pem_start);
-//     ESP_LOGI(TAG, "Uploading %d bytes", (int)data_len);
-
-//     esp_http_client_handle_t client = esp_http_client_init(&config);
-//     esp_http_client_set_url(client, url);
-//     esp_http_client_set_method(client, HTTP_METHOD_PUT);
-//     esp_http_client_set_header(client, "Content-Type", "application/json");
-
-//     if (esp_http_client_open(client, data_len) != ESP_OK) {
-//         ESP_LOGE(TAG, "open() failed");
-//         esp_http_client_cleanup(client);
-//         vTaskDelay(20 / portTICK_PERIOD_MS);
-//         return ESP_FAIL;
-//     }
-
-//     // Write data in a loop to handle partial writes
-//     size_t total_written = 0;
-//     int written = 0; 
-
-//     while (total_written < data_len) {
-//         written = esp_http_client_write(client, data + total_written, data_len - total_written);
-//         if (written < 0) {
-//             ESP_LOGE(TAG, "HTTP write failed: %d", written);
-//             break;
-//         }
-//         total_written += written;
-//     }
-
-//     esp_err_t err = esp_http_client_perform(client);
-//     if (err == ESP_OK) {
-//         ESP_LOGI(TAG, "HTTPS PUT Status = %d, content_length = %lld",
-//                  esp_http_client_get_status_code(client),
-//                  esp_http_client_get_content_length(client));
-//     } else {
-//         ESP_LOGE(TAG, "HTTPS PUT request failed: %s", esp_err_to_name(err));
-//     }
-
-//     esp_http_client_cleanup(client);
-//     vTaskDelay(20 / portTICK_PERIOD_MS);
-//     return err;
-// }
-
 esp_err_t https_put_stream(const char *url, const char *path, size_t fsize)
 {
     esp_http_client_config_t cfg = {
