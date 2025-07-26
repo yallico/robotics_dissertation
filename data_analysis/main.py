7717b727bb3a75df2798e3a99b325687623b865e
Luis Yallico <yallico92@gmail.com>
2025-07-26 20:49:26 +0100

    3 bot exp update 4

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -186,7 +186,7 @@ messages_df, logs_df, metadata_df = ingest_data('data_analysis/temp/data')
 in_scope = metadata_df[
     (metadata_df['num_robots'] == 3) &
     (metadata_df['robot_speed'] == 0.0) &
-    (metadata_df['topology'] == 0) &
+    (metadata_df['topology'] == 1) &
     (metadata_df['msg_limit'] == 0) &
     (metadata_df['max_genes'] == 10) &
     (metadata_df['migration_frequency'] == 0) &
